3626aa8ff2a8a94faaad1dc72b22874253ab2e00
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-09-12 22:59:24 +0530

    Add files via upload

---
+++ Variables.java/Variables.java
@@ -0,0 +1,21 @@
+class Variables
+{
+    public static void main (String a[])
+    {
+
+        int num = 4;
+        int num1 = 1;
+        int num2 = 2;
+
+        int num3 = 3;
+        int num4 = 5;
+
+        int result = num3 + num4;
+
+        System.out.println(3 + 5);
+        System.out.println(8 + 7);
+        System.out.println(num);
+        System.out.println(num1 + num2);
+        System.out.println(result);
+    }
+}
