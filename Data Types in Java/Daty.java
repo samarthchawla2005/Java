5f2cf190b75445d1dad28fe10420922a71b58dbd
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-09-13 23:12:17 +0530

    Add files via upload

---
+++ Data Types in Java/Daty.java
@@ -0,0 +1,23 @@
+class Daty
+{         //Daty => data type short form (Meine khud banaya)
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l; //Put l at the end of each long element
+        float f = 5.8f; //Put f at the end of each float element
+        double d = 5.8;
+        char c = 'k';
+        boolean b = true;
+
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.print(b);
+    }
+}
