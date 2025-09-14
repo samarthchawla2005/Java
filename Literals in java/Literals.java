a05a344909b115e252227204cd49eb187a8116d1
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-09-14 22:59:18 +0530

    Add files via upload

---
+++ Literals in java/Literals.java
@@ -0,0 +1,22 @@
+class Literals
+{
+    public static void main(String a[])
+    {
+        //Literals
+
+        int num1 = 0b101; //binary Number
+        int num2 = 0x7E; //Hexa Decimal Number
+        int num3 = 10_00_00_000; //Number of zeros
+        double num4 = 56;
+        double num5 = 12e10;
+        char c = 'c';
+        c++; //Increment
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(c);
+    }
+}
