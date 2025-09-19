5c1fb74defcdcb794bc221c6b7f09a0bd5a52366
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-09-19 20:57:55 +0530

    Add files via upload

---
+++ Operators in Java/Operators.java
@@ -0,0 +1,53 @@
+class Operators{
+    public static void main(String a[]){
+        int num1 = 7;
+        // int num2 = 5;
+
+        // int num3 = 8;
+
+        // int sum = num1 + num2;  //Addition
+        // int sub = num1 - num2;  //Substraction
+        // int pro = num1 * num2;  //Multiplication
+        // float div = num1 / num2;    //Division
+        // float rem = num1 % num2;    //Remainder
+
+        // System.out.println(sum);
+        // System.out.println(sub);
+        // System.out.println(pro);
+        // System.out.println(div);
+        // System.out.println(rem);
+
+        
+    //Self assigning value after changes
+        // num3 = num3 + 2;
+
+        //Adding same digit
+        // num3 += num3;
+
+        //Substracting same digit
+        // num3 -= num3;
+
+        //Multiplying same digit
+        // num3 *= num3;
+
+        //Dividing same digit
+        //  num3 /= num3;
+
+    //Increment operator
+        // num3++;              //Post Increment  //First Step - Fetching Value & Second Step - Incrementation
+        // ++num3;              //Pre Increment   //First Step - Incrementation & Second Step - Fetching Value
+    
+    //Decrement operator
+        // num3--;              //Post Decrement   //First Step - Fetching Value & Second Step - Decrementation
+        // --num3;              //Pre Decrement    //First Step - Decrementation & Second Step - Fetching Value
+
+
+    // int result = ++num1;
+
+    int result = num1++; 
+
+        // System.out.println(num3);
+
+        System.out.println(result);
+    }
+}
