63ba10005eb664706e3a97adbaeaa5b9aad91510
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-09-25 07:04:21 +0530

    Add files via upload

---
+++ Logical Operators in Java/Practice.java
@@ -0,0 +1,30 @@
+public class Practice{
+    public static void main(String[] args){
+        int x = 7;
+        int y = 5;
+        // int a = 8;
+        int a = 5;
+        int b = 9;
+
+        // boolean result = x > y ;
+
+//AND Operator        
+        // boolean result = x > y && a > b;
+        // boolean result = x > y && a < b;
+        
+//OR Operator
+
+        // boolean result = x > y || a<b;
+        // boolean result = x < y || a < b;
+        // boolean result = x < y || a < b || a > 1;
+
+        // System.out.println(result);
+        
+//NOT Operator
+
+        boolean result = a > b; //For a = 5
+
+        System.out.println(!result);
+
+    }
+}
