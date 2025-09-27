022cae8bada91d687be2f9b459b4060ab6eef1e6
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-09-27 16:03:39 +0530

    Add files via upload

---
+++ Conditional Statement ( If - else )/Program.java
@@ -0,0 +1,24 @@
+public class Program {
+    public static void main(String a[]) {
+        // int x = 8;
+        // int x = 18;
+        int x = 28;
+
+        // if (x > 10) {
+        //     System.out.println("Hello");
+        // } else {
+        //     System.out.println("Bye");
+        // }
+
+        // if(true){
+        //     System.out.println("Hello");
+        // } 
+
+        if(x>10 && x<20){
+            System.out.println("Hello");
+        }
+        else{
+            System.out.println("Bye");
+        }
+    }
+}
---
+++ Conditional Statement ( If - else )/Program_2.java
@@ -0,0 +1,13 @@
+public class Program_2 {
+    public static void main(String[] args) {
+        // int x = 5;
+        int x = 8;
+        int y = 7;
+
+        if (x>y) {
+            System.out.println(x);
+        } else {
+            System.out.println(y);            
+        }
+    }
+}
