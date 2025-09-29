4ba3fa42ac7035e3df23b92bb02a9cd6abce23a2
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-09-29 19:40:14 +0530

    Add files via upload

---
+++ Conditional Statement (If - else - If)/Program.java
@@ -0,0 +1,19 @@
+public class Program {
+    public static void main(String[] args){
+        int x = 8;
+        int y = 9;
+        int z = 6;
+
+                 
+        if(x>y && x>z){
+            System.out.println("x is greatest");
+        }
+        else if(y>x && y>z){
+            System.out.println("y is greatest");
+        }
+        else{
+            System.out.println("z is greatest");
+        }
+        
+    }
+}
