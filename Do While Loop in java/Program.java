60582561cffd118810346841c7f541d4d0f669cf
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-10-07 10:15:59 +0530

    Add files via upload

---
+++ Do While Loop in java/Program.java
@@ -0,0 +1,12 @@
+public class Program{
+    public static void main(String a[]){
+
+        // int i = 1;
+        int i = 5;
+
+        do{
+            System.out.println("Hi " + i);
+            i++;
+        }while(i<=4);
+    }
+}
