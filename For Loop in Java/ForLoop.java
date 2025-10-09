799272cba6888204360531e9cb0825e0d60eb639
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-10-09 09:01:46 +0530

    Add files via upload

---
+++ For Loop in Java/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop{
+    public static void main (String[] args){
+        int i = 1;
+        for( ; i<=5 ; ){
+            System.out.println("Hello World " + i);
+            i++;
+        }
+    }
+}
