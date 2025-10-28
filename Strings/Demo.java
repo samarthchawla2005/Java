04caaa1d4c0c924ec7736e53130b09459085e9a7
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-10-28 09:20:09 +0530

    Strings in Java

---
+++ Strings/Demo.java
@@ -0,0 +1,29 @@
+public class Demo{
+    public static void main (String a[]){
+        // String name = new String("Samarth");
+        
+        String name = "Samarth";
+
+        System.out.println(name + " Chawla");
+
+        // System.out.println(name);
+
+        //Hash Code
+        // System.out.println(name.hashCode());
+
+        //Concatenation
+        // System.out.println("Hello " + name);
+
+        //Method;
+        // System.out.println(name.charAt(0));
+        // System.out.println(name.charAt(1));
+        // System.out.println(name.charAt(2));
+        // System.out.println(name.charAt(3));
+        // System.out.println(name.charAt(4));
+        // System.out.println(name.charAt(5));
+        // System.out.println(name.charAt(6));
+
+        //Strings Concatination
+        // System.out.println(name.concat(" Chawla"));
+    }
+}
