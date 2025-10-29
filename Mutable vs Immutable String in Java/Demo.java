e1752963ea11a184e7cb2e212d2a2d84ce04b5ef
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-10-29 06:12:38 +0530

    Mutable & Immutable String in Java

---
+++ Mutable vs Immutable String in Java/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args) {   
+        String name = "Samarth";
+        name = name + " Chawla";
+        System.out.println("hello " + name);
+
+        String s1 = "Samarth";
+        String s2 = "Samarth";
+
+        System.out.println(s1==s2);
+    }
+}
