5b93e438413ccef434f7648fd399bd287a3f75e8
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-11-03 09:29:57 +0530

    Java String Buffer Concept
    The StringBuffer class in Java represents a mutable sequence of characters, meaning its content can be modified after creation. This contrasts with the String class, which creates immutable objects where any modification results in a new String object.

---
+++ StringBuffer & StringBuilder in Java/Demo.java
@@ -0,0 +1,17 @@
+public class Demo{
+    public static void main(String[] args) {
+        // StringBuffer sb = new StringBuffer("Sam");
+        StringBuffer sb = new StringBuffer("Samarth");
+        // System.out.println("Hello " + sb);
+        // System.out.println(sb.capacity());
+        // System.out.println(sb.length());
+        // sb.append(" Chawla");
+        
+        // String str = sb.toString();
+        // sb.deleteCharAt(2);
+        // sb.insert(0,  "Java King ");
+
+        sb.setLength(30);
+        System.out.println(sb);
+    }
+}
