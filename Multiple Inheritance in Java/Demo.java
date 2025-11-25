be16ef31f603d363dc73fce516e9d600ed69dd94
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-11-25 18:20:07 +0530

    Add files via upload

---
+++ Multiple Inheritance in Java/Demo.java
@@ -0,0 +1,30 @@
+class A{
+
+}
+
+class B extends A{
+
+}
+
+class C{
+    
+}
+
+public class Demo{
+    public static void main(String a[]){
+        // Calc obj = new Calc();
+        //AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
+        int r1 = obj.add(4,5);
+        int r2 = obj.sub(7,3);
+        int r3 = obj.multi(2,10);
+        int r4 = obj.div(22,11);
+        double r5 = obj.power(4,2);
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+    }
+}
