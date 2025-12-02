970f42218cbf2a042c1ffffcae19085e05c0bc2a
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-12-02 19:17:06 +0530

    Add files via upload

---
+++ Packages in Java/Demo.java
@@ -0,0 +1,20 @@
+// import tools.Calc;
+
+// import java.util.ArrayList;
+
+// import tools.AdvCalc;
+
+import other.*;
+import other.tools.*; // * reapresents all files included in a folder (in never include folders inside anotehr folders)
+
+public class Demo {
+    public static void main(String a[]) {
+
+        // ArrayList list = new ArrayList();
+
+        Calc obj = new Calc();
+        AdvCalc obj1 = new AdvCalc();
+
+        A obj2 = new A();
+    }
+}
