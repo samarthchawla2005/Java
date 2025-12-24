d5bc4b5aea39043cbec9ef595c0070e9ce34c34a
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-12-24 22:16:33 +0530

    Add files via upload

---
+++ Annotation in Java/Demo.java
@@ -0,0 +1,19 @@
+class A {
+    public void showTheDataWhichBelongsToThisClass() {
+        System.out.println("in A show");
+    }
+}
+
+class B extends A {
+    @Override
+    public void showTheDataWhichBelongsToThisClass() {
+        System.out.println("in B show");
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.showTheDataWhichBelongsToThisClass();
+    }
+}
