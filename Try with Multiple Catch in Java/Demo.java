ef1344d4c4930f94f83a8466c423b402f928e737
SAMARTH CHAWLA <73781310+samarthchawla2005@users.noreply.github.com>
2025-12-30 19:05:18 +0530

    Try with Multiple Catch in Java

---
+++ Try with Multiple Catch in Java/Demo.java
@@ -0,0 +1,35 @@
+public class Demo {
+    public static void main(String[] args) {
+        // int i = 9;
+        // int i = 0;
+        int i = 2;
+        int j = 0;
+        int nums[] = new int[5];
+        String str = null;
+
+        try
+        {
+            j = 18 / i;
+            System.out.println(str.length());
+            System.out.println(nums[1]);
+            System.out.println(nums[5]);
+        }
+        // catch(Exception e)
+        catch(ArithmeticException e)
+        {
+            // System.out.println("Something Went Wrong " + e);
+            System.out.println("Cannot divide by zero");
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Stay in your limit");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Something went wrong... " + e);
+        }
+
+        System.out.println(j);
+        System.out.println("Exit");
+    }
+}
